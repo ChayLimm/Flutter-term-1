9c66bd388d08cfaa5dbb905031910c7910c46109
ChayLimm <156899992+ChayLimm@users.noreply.github.com>
2024-11-06 07:06:22 +0700

    submitting stateful

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ import 'package:flutter/material.dart';
 // import 'package:lesson/W4_S1/EX1/main.dart';
 // import 'package:lesson/W4_S1/EX2/main.dart';
 // import 'package:lesson/W4_S1/EX3/main.dart';
-import 'package:lesson/W4_S1/challenge/main.dart';
-
+// import 'package:lesson/W4_S1/challenge/main.dart';
+// import 'package:lesson/lessonOfThwDay/rolldice.dart';
+// import 'package:lesson/lessonOfThwDay/discountCard.dart';
+// import 'package:lesson/W4_S3/EX1/main.dart';
+// import 'package:lesson/W4_S3/EX2/main.dart';
+// import 'package:lesson/W4_S3/EX3/main.dart';
+import 'package:lesson/W4_S3/challenge/main.dart';
 
 
 void main() {
@@ -19,14 +24,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return
+    
+    MaterialApp(
       debugShowCheckedModeBanner: false,
+      
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
